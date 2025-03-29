5cc756271f9fd83fb487adaa3256f82b9c487edc
Phil <irons7654321@gmail.com>
2025-03-29 13:25:58 +0800

    Initial Commit

---
+++ GoodSleeperCountingSheep-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodSleeperCountingSheep-swift
+//
+//  Created by Phil on 2025/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoodSleeperCountingSheep-swiftTests/GoodSleeperCountingSheep_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoodSleeperCountingSheep_swiftTests.swift
+//  GoodSleeperCountingSheep-swiftTests
+//
+//  Created by Phil on 2025/3/29.
+//
+
+import Testing
+@testable import GoodSleeperCountingSheep_swift
+
+struct GoodSleeperCountingSheep_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
